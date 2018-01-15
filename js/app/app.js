6fe7007fdbe424b2ecfaa8c45904f3e31b3cf5b9
ahmed elmessiry <messiry.ahmed@gmail.com>
2018-01-15 20:24:31 +0200

    HTML structure for the map page

--- js/app/app.js
+++ js/app/app.js
@@ -7,13 +7,7 @@
 // Here's my data model
 function ViewModel() {
 
-    this.firstName = ko.observable('ahmed');
-    this.lastName = ko.observable('elmessiry');
-
-    this.fullName = ko.pureComputed(function() {
-        // Knockout tracks dependencies automatically. It knows that fullName depends on firstName and lastName, because these get called when evaluating fullName.
-        return this.firstName() + " " + this.lastName();
-    }, this);
+    
 };
 
 ko.applyBindings(new ViewModel());
