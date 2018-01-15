4c7ba48b37b2a69c5e1ce7bb13a5042706e863c7
ahmed elmessiry <messiry.ahmed@gmail.com>
2018-01-15 20:01:16 +0200

    Testing ko bindings & observables

--- js/app/app.js
+++ js/app/app.js
@@ -2,9 +2,18 @@
 
 
 /*  ***************   View Model   *******************  */
-function ViewModel(){
-    alert('hi there');
-}
 
 
-ko.applyBindings( new ViewModel() )
+// Here's my data model
+function ViewModel() {
+
+    this.firstName = ko.observable('ahmed');
+    this.lastName = ko.observable('elmessiry');
+
+    this.fullName = ko.pureComputed(function() {
+        // Knockout tracks dependencies automatically. It knows that fullName depends on firstName and lastName, because these get called when evaluating fullName.
+        return this.firstName() + " " + this.lastName();
+    }, this);
+};
+
+ko.applyBindings(new ViewModel());
