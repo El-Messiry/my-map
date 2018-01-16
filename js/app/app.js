5c945271b57c0e55b2bf284c921407bf72dffd60
ahmed elmessiry <messiry.ahmed@gmail.com>
2018-01-16 16:00:03 +0200

    layout complete , waiting to add map

--- js/app/app.js
+++ js/app/app.js
@@ -10,9 +10,11 @@ function ViewModel() {
     // toggling nav item hamburger
     this.toggle_nav_icon = function(){
         $('.nav-icon').toggleClass('active-nav-icon');
+        $('.sidebar').toggleClass('active-side-bar');
+        $('.page-content').toggleClass('active-side-bar');
     }
 
-    
+
 };
 
 ko.applyBindings(new ViewModel());
