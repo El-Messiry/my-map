ba21406112d5312503521c7830942793552d34fb
ahmed elmessiry <messiry.ahmed@gmail.com>
2018-01-18 17:49:29 +0200

    map is loaded with markers - g-map in separate script

--- js/app/app.js
+++ js/app/app.js
@@ -6,13 +6,30 @@
 
 // Here's my data model
 function ViewModel() {
-
+    self = this ;
     // toggling nav item hamburger
-    this.toggle_nav_icon = function(){
+    self.toggle_nav_icon = function(){
         $('.nav-icon').toggleClass('active-nav-icon');
         $('.sidebar').toggleClass('active-side-bar');
         $('.page-content').toggleClass('active-side-bar');
-    }
+    };
+
+    var btn_txt = {
+        show_txt : 'Show Places' ,
+        hide_txt : 'Hide Places' ,
+    };
+
+    self.map_btn_txt = ko.observable(btn_txt.show_txt);
+
+
+    self.toggle_map = function(){
+        $('#map-btn').toggleClass('active-btn');
+        if (self.map_btn_txt() == btn_txt.hide_txt) {
+            self.map_btn_txt(btn_txt.show_txt);
+        } else {
+            self.map_btn_txt(btn_txt.hide_txt);
+        }
+    };
 
 
 };
